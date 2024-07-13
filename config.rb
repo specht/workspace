fdf2079435d5d1d8d4ca0310fbf4dc7d43a6e492
Michael Specht <micha.specht@gmail.com>
2024-07-13 15:42:36 +0200

    yay

--- config.rb
+++ config.rb
@@ -197,11 +197,11 @@ docker_compose[:services].values.each do |x|
     x[:network_mode] = 'default'
 end
 
-docker_compose[:networks] = {
-    :workspace => {
-        :name => 'workspace',
-    }
-}
+# docker_compose[:networks] = {
+#     :workspace => {
+#         :name => 'workspace',
+#     }
+# }
 
 if DEVELOPMENT
     docker_compose[:services][:nginx][:ports] = ["0.0.0.0:#{DEV_NGINX_PORT}:80"]
