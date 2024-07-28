a38f144c8d80dd03f12e3e19e6585ebc90b95f03
Michael Specht <micha.specht@gmail.com>
2024-07-28 12:35:25 +0200

    yay

--- src/content/clisp/factor.lisp
+++ src/content/clisp/factor.lisp
@@ -1,21 +1,36 @@
-(defun prime-factors (n)
-  (let ((factors '())) ; Initialize factors as an empty list
-    (loop for i from 2 to (isqrt n)
-          do (loop while (zerop (mod n i))
-                   do (progn
-                        (push i factors)
-                        (setf n (/ n i)))))
-    (if (> n 1)
-        (push n factors))
-    (nreverse factors))) ; Reverse the list to maintain the order of factors
+#!/usr/bin/env clisp
+(defun prime-factors (number &optional (return-values '()))
+  ;; function declaration has an optional return-values parameter for
+  ;; tail-recursive use
+  (when (> number 0)
+    ;; no prime factors for negative numbers
+    (loop with max-divisor = (isqrt number)
+          ;; loop through possible divisors
+          ;; stop at divisor = square root of number
+          for divisor = 2
+            then (if (evenp divisor)
+                     ;; if divisor is even, increment
+                     ;; if not, add two
+                     (1+ divisor) (+ divisor 2))
+          do (cond ((> divisor max-divisor)
+                    ;; have we checked all possible divisors but none fit?
+                    ;; number is prime => return with any return-values
+                    (return (cons (list number 1) return-values)))
+                   ((zerop (rem number divisor))
+                    ;; is number evenly divisible by divisor?
+                    ;; we have found a factor of the number.
+                    ;; recursively call prime-factors
+                    (return
+                      (prime-factors
+                       ;; use number/divisor as new number
+                       (truncate number divisor)
+                       ;; add factor to return-values list (or increment
+                       ;; counter if more sensible)
+                       (if (eq divisor (caar return-values))
+                           (cons 
+                            (list (caar return-values) (1+ (cadar return-values)))
+                            (cdr return-values))
+                           (cons (list divisor 1) return-values)))))))))
 
-(defun read-number ()
-  (format *query-io* "Enter a number: ")
-  (force-output *query-io*)
-  (parse-integer (read-line *query-io*)))
-
-(defun main ()
-  (let ((number (read-number)))
-    (format t "Prime factors of ~a: ~a~%" number (prime-factors number))))
-
-(main)
+(format T "Please enter the number to factorise: ")
+(format T "Prime factors: ~d" (prime-factors (read)))
