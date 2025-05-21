4e73254cb8d686f5684ccd5f10a87194a9549165
Michael Specht <micha.specht@gmail.com>
2025-05-21 18:53:06 +0200

    yay

--- src/scripts/collect-music-releases/convert-masters.rb
+++ src/scripts/collect-music-releases/convert-masters.rb
@@ -7,23 +7,9 @@ require 'open3'
 require 'set'
 
 $wanted_artists = Set.new()
-$country_for_artist = {}
 
-File.open('wanted-artists.txt') do |f|
-    current_country = nil
-    f.each_line do |line|
-        line.strip!
-        next if line.empty?
-        break if line == '__END__'
-        if line[0] == '#'
-            line = line[1, line.size - 1].strip
-            current_country = line
-        else
-            artist_id = line.sub('https://www.discogs.com/artist/', '').split('-')[0]
-            $wanted_artists << artist_id
-            $country_for_artist[artist_id] = current_country
-        end
-    end
+Dir['cache/artists/*.xml'].each do |file|
+    $wanted_artists << File.basename(file, '.xml')
 end
 
 def handle_xml(xml)
