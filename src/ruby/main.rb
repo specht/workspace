2274f1f7e6b6645b564297b21a8b396e746080b1
Michael Specht <micha.specht@gmail.com>
2025-05-24 22:14:40 +0200

    yay

--- src/ruby/main.rb
+++ src/ruby/main.rb
@@ -324,6 +324,28 @@ class Main < Sinatra::Base
                         proxy_set_header Accept-Encoding gzip;
                         proxy_pass http://#{running_servers[fs_tag][:ip]}:8443;
                     }
+                    location /#{server_tag}/neo4j {
+                        if ($cookie_server_sid != "#{user[:server_sid]}") {
+                            return 403;
+                        }
+                        rewrite ^/#{server_tag}/neo4j(.*)$ $1 break;
+                        proxy_set_header Host $http_host;
+                        proxy_set_header Upgrade $http_upgrade;
+                        proxy_set_header Connection upgrade;
+                        proxy_set_header Accept-Encoding gzip;
+                        proxy_pass http://#{running_servers[fs_tag][:ip]}:7474;
+                    }
+                    location /#{server_tag}/bolt {
+                        # if ($cookie_server_sid != "#{user[:server_sid]}") {
+                        #     return 403;
+                        # }
+                        rewrite ^/#{server_tag}/bolt(.*)$ $1 break;
+                        proxy_set_header Host $http_host;
+                        proxy_set_header Upgrade $http_upgrade;
+                        proxy_set_header Connection upgrade;
+                        proxy_set_header Accept-Encoding gzip;
+                        proxy_pass http://#{running_servers[fs_tag][:ip]}:7687;
+                    }
                     END_OF_STRING
                     if test.nil?
                         if user[:share_tag] && user[:share_tag] =~ /^[a-z0-9]{48}$/
@@ -1238,9 +1260,8 @@ class Main < Sinatra::Base
 
             network_name = "bridge"
             mysql_ip = `docker inspect workspace_mysql_1`.split('"IPAddress": "')[1].split('"')[0]
-            postgres_ip = `docker inspect workspace_postgres_1`.split('"IPAddress": "')[1].split('"')[0]
             login = email.split('@').first.downcase
-            command = "docker run --add-host=mysql:#{mysql_ip} --add-host=postgres:#{postgres_ip} --cpus=2 -d --rm -e PUID=1000 -e GUID=1000 -e TZ=Europe/Berlin -e DEFAULT_WORKSPACE=/workspace -e MYSQL_HOST=\"mysql\" -e MYSQL_USER=\"#{login}\" -e MYSQL_PASSWORD=\"#{Main.gen_password_for_email(email, MYSQL_PASSWORD_SALT)}\" -e MYSQL_DATABASE=\"#{login}\" -e POSTGRES_HOST=\"postgres\" -e POSTGRES_USER=\"#{login}\" -e POSTGRES_PASSWORD=\"#{Main.gen_password_for_email(email, POSTGRES_PASSWORD_SALT)}\" -e POSTGRES_DATABASE=\"#{login}\" -v #{PATH_TO_HOST_DATA}/user/#{container_name}/config:/config -v #{PATH_TO_HOST_DATA}/user/#{container_name}/workspace:/workspace --network #{network_name} #{test_tag ? '-v /dev/null:/etc/resolv.conf:ro' : ''} --name hs_code_#{container_name} hs_code_server"
+            command = "docker run --add-host=mysql:#{mysql_ip} --cpus=2 -d --rm -e PUID=1000 -e GUID=1000 -e TZ=Europe/Berlin -e DEFAULT_WORKSPACE=/workspace -e MYSQL_HOST=\"mysql\" -e MYSQL_USER=\"#{login}\" -e MYSQL_PASSWORD=\"#{Main.gen_password_for_email(email, MYSQL_PASSWORD_SALT)}\" -e MYSQL_DATABASE=\"#{login}\" -e POSTGRES_HOST=\"postgres\" -e POSTGRES_USER=\"#{login}\" -e POSTGRES_PASSWORD=\"#{Main.gen_password_for_email(email, POSTGRES_PASSWORD_SALT)}\" -e POSTGRES_DATABASE=\"#{login}\" -v #{PATH_TO_HOST_DATA}/user/#{container_name}/config:/config -v #{PATH_TO_HOST_DATA}/user/#{container_name}/workspace:/workspace --network #{network_name} #{test_tag ? '-v /dev/null:/etc/resolv.conf:ro' : ''} --name hs_code_#{container_name} hs_code_server"
             STDERR.puts command
             system(command)
 
