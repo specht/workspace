b0fa052a8b6a57016cabd95892e45fb60fa6444e
Michael Specht <micha.specht@gmail.com>
2025-07-30 19:36:25 +0200

    yay

--- build-image.sh
+++ build-image.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl -sL https://storage.googleapis.com/dart-archive/channels/stable/release/3.8.2/linux_packages/dart_3.8.2-1_amd64.deb -o docker/code/dart.deb
+wget -N -O docker/code/dart.deb https://storage.googleapis.com/dart-archive/channels/stable/release/3.8.2/linux_packages/dart_3.8.2-1_amd64.deb
 cd docker/code && docker build -t hs_code_server .
