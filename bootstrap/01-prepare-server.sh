cb4bb96359d6b512781fc2bd9473bfd733068b1a
Michael Specht <micha.specht@gmail.com>
2025-07-31 16:09:14 +0200

    yay

--- bootstrap/01-prepare-server.sh
+++ bootstrap/01-prepare-server.sh
@@ -2,6 +2,6 @@
 set -e
 
 dnf install -y ruby
-wget https://raw.githubusercontent.com/specht/workspace/refs/heads/master/bootstrap/02-setup-server.rb
-wget https://raw.githubusercontent.com/specht/workspace/refs/heads/master/bootstrap/config.yaml
+wget -q https://raw.githubusercontent.com/specht/workspace/refs/heads/master/bootstrap/02-setup-server.rb -O 02-setup-server.rb
+wget -q https://raw.githubusercontent.com/specht/workspace/refs/heads/master/bootstrap/config.yaml -O config.yaml
 chmod +x 02-setup-server.rb
