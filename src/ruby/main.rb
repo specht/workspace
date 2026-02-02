631f6ad6034420f080e6aef5ecaa5816463a0708
Michael Specht <micha.specht@gmail.com>
2026-02-02 11:17:21 +0100

    yay

--- src/ruby/main.rb
+++ src/ruby/main.rb
@@ -428,7 +428,7 @@ class Main < Sinatra::Base
                     set $port "";
                     if ($http_host ~* :(?<hp>\\d+)$) { set $port :$hp; }
 
-                    return 302 http://${t}p${p}.#{WEBSITE_HOST.split(':').first}$port$r;
+                    return 302 http://${t}-${p}.#{WEBSITE_HOST.split(':').first}$port$r;
                 }
 
                 location / {
@@ -441,11 +441,11 @@ class Main < Sinatra::Base
             }
 
             # ==========================================
-            # Port subdomains: <tag>p<port>.#{WEBSITE_HOST.split(':').first}
+            # Port subdomains: <tag>-<port>.#{WEBSITE_HOST.split(':').first}
             # ==========================================
             server {
                 listen 80;
-                server_name ~^(?<t>[a-z0-9]+)p(?<p>\\d+)\.#{WEBSITE_HOST.split(':').first.gsub('.', '\.')}$;
+                server_name ~^(?<t>[a-z0-9]+)-(?<p>\\d+)\.#{WEBSITE_HOST.split(':').first.gsub('.', '\.')}$;
                 client_max_body_size 100M;
                 access_log /var/log/nginx/access.log custom;
                 charset utf-8;
