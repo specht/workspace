f0fa9551dca3aaf226356e6e326fba8a6663359b
Michael Specht <micha.specht@gmail.com>
2026-02-03 10:19:00 +0100

    yay

--- src/ruby/housekeeping.rb
+++ src/ruby/housekeeping.rb
@@ -3,7 +3,7 @@
 require 'json'
 
 running_servers = []
-inspect = JSON.parse(`docker network inspect bridge`)
+inspect = JSON.parse(`docker network inspect workspace_internal`)
 inspect.first['Containers'].values.each do |container|
     name = container['Name']
     next unless name[0, 8] == 'hs_code_'
