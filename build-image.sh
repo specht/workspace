8df9e11b827b99ea1a7a41e796de45af3c4a2c3f
Michael Specht <micha.specht@gmail.com>
2026-07-25 10:27:24 +0200

    yay

--- build-image.sh
+++ build-image.sh
@@ -3,6 +3,7 @@ docker build -t hackschule-exec-ruby src/codebites/executor/ruby
 docker build -t hackschule-exec-python src/codebites/executor/python
 docker build -t hackschule-exec-javascript src/codebites/executor/javascript
 wget -N -O docker/code/dart.deb https://storage.googleapis.com/dart-archive/channels/stable/release/3.8.2/linux_packages/dart_3.8.2-1_amd64.deb
-cp src/sidebar-init/hackschule-sidebar-init-0.0.1.vsix docker/code/hackschule-sidebar-init-0.1.0.vsix
-cp src/checkpoints/hackschule-checkpoints-0.6.0.vsix docker/code/hackschule-checkpoints-0.6.0.vsix
+cp src/vscode-extensions/sidebar-init/hackschule-sidebar-init-0.0.1.vsix docker/code/hackschule-sidebar-init-0.1.0.vsix
+cp src/vscode-extensions/checkpoints/hackschule-checkpoints-0.6.0.vsix docker/code/hackschule-checkpoints-0.6.0.vsix
+cp src/vscode-extensions/keyboard-tutorial/hackschule-keyboard-tutorial-0.1.0.vsix docker/code/hackschule-keyboard-tutorial-0.1.0.vsix
 docker build -t hs_code_server docker/code
