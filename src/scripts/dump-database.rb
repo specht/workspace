e8100357fca4a7ff15ed2d31f5d572ea5b114b20
Michael Specht <micha.specht@gmail.com>
2026-08-15 22:30:59 +0200

    yay

--- src/scripts/dump-database.rb
+++ src/scripts/dump-database.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 
 progress_mode = STDERR.tty? ? 'pretty' : 'plain'
-system("cd ../.. && ./config.rb exec ruby env NEO4J_BOLT_PROGRESS=#{progress_mode} neo4j_bolt --host neo4japp:7687 dump #{ARGV.map { |x| '\"' + x + '\"' }.join(' ') }&& cd src/scripts")
+system("cd ../.. && ./config.rb exec ruby env NEO4J_BOLT_PROGRESS=#{progress_mode} neo4j_bolt --host neo4japp:7687 dump --color bright-magenta #{ARGV.map { |x| '\"' + x + '\"' }.join(' ') }&& cd src/scripts")
--- src/scripts/load-dump.rb
+++ src/scripts/load-dump.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 
 progress_mode = STDERR.tty? ? 'pretty' : 'plain'
-system("cd ../.. && cat \"#{File.expand_path(ARGV.first)}\" | docker exec -i $(./config.rb ps -q ruby) env NEO4J_BOLT_PROGRESS=#{progress_mode} neo4j_bolt --host neo4japp:7687 load /dev/stdin #{(ARGV[1, ARGV.size - 1] || []).join(' ')} && cd src/scripts")
+system("cd ../.. && cat \"#{File.expand_path(ARGV.first)}\" | docker exec -i $(./config.rb ps -q ruby) env NEO4J_BOLT_PROGRESS=#{progress_mode} neo4j_bolt --host neo4japp:7687 load --color bright-magenta /dev/stdin #{(ARGV[1, ARGV.size - 1] || []).join(' ')} && cd src/scripts")
